5e37364ad3bd9cf5b85f153ba53d845f5a507c02
rcintron1 <rolando.cintron@gmail.com>
2022-01-23 20:40:17 -0500

    Feat: Reading keyboard input

--- main.go
+++ main.go
@@ -26,10 +26,25 @@ func main() {
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
+		_, err = fmt.Scan(&originValue)
+		if err != nil {
+			printError(errReadingInput)
+		}
+		if originUnit == "C" {
+			convertToFahrenheit(originValue)
+		} else {
+			convertToCelsius(originValue)
+		}
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 
-		if shouldConvertAgain != "Y" {
+		_, err = fmt.Scanln(&shouldConvertAgain)
+
+		if err != nil {
+			printError(errReadingInput)
+		}
+
+		if strings.ToUpper(shouldConvertAgain) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
